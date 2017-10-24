3f4f4ed06b8d4e99d605c56e61a778710bd0f2f1
ah100101 <ah100101@gmail.com>
2017-10-24 00:23:49 -0500

    wiring up apollo

---
+++ src/js/graphql/apolloProvider.js
@@ -0,0 +1,20 @@
+// let Vue = require('../vendor/vue')
+// let { ApolloClient, createBatchingNetworkInterface } = require('apollo-client')
+// let VueApollo = require('vue-apollo')
+// let config = require('./graphql.json')
+// console.log(VueApollo)
+// // TODO: hook up config
+// const apolloClient = new ApolloClient({
+//   networkInterface: createBatchingNetworkInterface({
+//     uri: '__SIMPLE_API_ENDPOINT__'
+//   }),
+//   connectToDevTools: true
+// })
+
+// Vue.use(VueApollo)
+
+// const apolloProvider = new VueApollo({
+//   defaultClient: apolloClient
+// })
+
+// module.exports = apolloProvider
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,33 @@ let Vue = require('./vendor/vue')
 let Store = require('./state/store.js')
 let App = require('./components/App.vue')
 let Router = require('./routing/router.js')
+// let ApolloProvider = require('./graphql/apolloProvider.js')
+
+// console.log(ApolloProvider)
+
+let { ApolloClient, createBatchingNetworkInterface } = require('apollo-client')
+let VueApollo = require('vue-apollo')
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  networkInterface: createBatchingNetworkInterface({
+    uri: 'http://localhost:3020/graphql',
+  }),
+  connectToDevTools: true,
+})
+
+// Install the vue plugin
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo.ApolloProvider({
+  defaultClient: apolloClient,
+})
 
 new Vue({
   el: '#app',
   store: Store,
   router: Router,
+  apolloProvider,
   components: {
     app: App
   }
