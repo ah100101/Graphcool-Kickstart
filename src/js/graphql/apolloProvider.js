2d05a8431f915a079c9cac1fce5a41f0d3be8080
ah100101 <ah100101@gmail.com>
2017-10-25 23:05:46 -0500

    updates to graphql files

--- src/js/graphql/apolloProvider.js
+++ src/js/graphql/apolloProvider.js
@@ -1,20 +1,19 @@
-// let Vue = require('../vendor/vue')
-// let { ApolloClient, createBatchingNetworkInterface } = require('apollo-client')
-// let VueApollo = require('vue-apollo')
-// let config = require('./graphql.json')
-// console.log(VueApollo)
-// // TODO: hook up config
-// const apolloClient = new ApolloClient({
-//   networkInterface: createBatchingNetworkInterface({
-//     uri: '__SIMPLE_API_ENDPOINT__'
-//   }),
-//   connectToDevTools: true
-// })
+let Vue = require('../vendor/vue')
+let { ApolloClient, createBatchingNetworkInterface } = require('apollo-client')
+let VueApollo = require('vue-apollo')
+let config = require('./graphql.json')
 
-// Vue.use(VueApollo)
+const apolloClient = new ApolloClient({
+  networkInterface: createBatchingNetworkInterface({
+    uri: config.simpleApiEndpoint
+  }),
+  connectToDevTools: config.useDevTools
+})
+  
+Vue.use(VueApollo)
+  
+const apolloProvider = new VueApollo.ApolloProvider({
+  defaultClient: apolloClient
+})
 
-// const apolloProvider = new VueApollo({
-//   defaultClient: apolloClient
-// })
-
-// module.exports = apolloProvider
+module.exports = apolloProvider
