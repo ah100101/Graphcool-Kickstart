494e4aed14a9283ac7635b46d56400cce8485f7f
ah100101 <ah100101@gmail.com>
2017-10-25 21:39:08 -0500

    some clean up

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,10 @@ let Vue = require('./vendor/vue')
 let Store = require('./state/store.js')
 let App = require('./components/App.vue')
 let Router = require('./routing/router.js')
-// let ApolloProvider = require('./graphql/apolloProvider.js')
-
-// console.log(ApolloProvider)
 
 let { ApolloClient, createBatchingNetworkInterface } = require('apollo-client')
 let VueApollo = require('vue-apollo')
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
   networkInterface: createBatchingNetworkInterface({
     uri: 'http://localhost:3020/graphql',
@@ -17,7 +13,6 @@ const apolloClient = new ApolloClient({
   connectToDevTools: true,
 })
 
-// Install the vue plugin
 Vue.use(VueApollo)
 
 const apolloProvider = new VueApollo.ApolloProvider({
