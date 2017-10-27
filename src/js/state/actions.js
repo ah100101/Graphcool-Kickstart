11307277306470d1d00b8dae8812c5fde81e358e
ah100101 <ah100101@gmail.com>
2017-10-26 23:51:05 -0500

    added static todo

--- src/js/state/actions.js
+++ src/js/state/actions.js
@@ -4,6 +4,15 @@ const actions = {
   },
   setAnakinThreat: (context, text) => {
     context.commit('setAnakinThreat', text)
+  },
+  setTasks: (context, tasks) => {
+    context.commit('setTasks', tasks)
+  },
+  setTasksCompleted: (context, tasks) => {
+    context.commit('setTasksCompleted', tasks)
+  },
+  addTask: (context, task) => {
+    context.commit('addTask', task)
   }
 }
 
