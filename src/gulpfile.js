e6c0d55ea7a6b9e2b74adb08a7327f2151e8676b
ah100101 <ah100101@gmail.com>
2017-10-29 21:08:16 -0500

    gitignore updates and gulpfile changes

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,36 +1,84 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
-var vueify = require('vueify')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const vueify = require('vueify')
+const del = require('del')
+const series = require('run-sequence')
 
-gulp.task('styles:deploy', function () {
-  gulp.src('scss/index.scss')
+const paths = {
+  del: {
+    folders: [ '../public' ]
+  },
+  html: {
+    src: './*.html',
+    dest: '../public'
+  },
+  scss: {
+    src: './scss/**/*.scss',
+    dest: '../public/css'
+  },
+  assets: {
+    src: './assets/**/*',
+    dest: '../public/assets'
+  },
+  scripts: {
+    src: './js/main.js',
+    dest: '../public/js',
+    name: 'main.js'
+  }
+}
+
+// HTML
+gulp.task('html', () =>
+  gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+)
+
+gulp.task('html:watch', ['html'], () =>
+  gulp.watch(paths.html.src, ['html'])
+)
+
+// Assets
+gulp.task('assets', () =>
+gulp.src(paths.assets.src)
+  .pipe(gulp.dest(paths.assets.dest))
+)
+
+gulp.task('assets:watch', ['assets'], () =>
+  gulp.watch(paths.assets.src, ['assets'])
+)
+
+// SCSS
+gulp.task('scss', () =>
+  gulp.src(paths.scss.src)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('../public/css/'))
-})
+    .pipe(gulp.dest(paths.scss.dest))
+)
 
-gulp.task('scripts:deploy', function () {
-  return browserify('./js/index.js')
+gulp.task('scss:watch', ['scss'], () =>
+  gulp.watch(paths.scss.src, ['scss'])
+)
+
+// Javascript
+gulp.task('scripts', () => {
+  return browserify(paths.scripts.src)
     .transform(vueify)
     .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('../public/js/'))
+    .pipe(source(paths.scripts.name))
+    .pipe(gulp.dest(paths.scripts.dest))  
 })
 
-gulp.task('scripts:watch', function () {
-  gulp.watch('./js/**', ['scripts:deploy'])
-})
+gulp.task('scripts:watch', ['scripts'], () =>
+  gulp.watch(paths.javascript.src, ['scripts'])
+)
 
-gulp.task('styles:watch', function () {
-  gulp.watch('./scss/**', ['styles:deploy'])
-})
+// Clean
+gulp.task('clean', () => del(paths.del.folders, {force: true}))
 
-gulp.task('fire:everything', function () {
-  gulp.start('scripts:deploy')
-  gulp.start('styles:deploy')
-})
+// Builds
+gulp.task('build', ['html', 'assets', 'scss', 'scripts'])
+gulp.task('watch', ['html:watch', 'assets:watch', 'scss:watch', 'scripts:watch'])
 
-gulp.task('watch:everything', function () {
-  gulp.watch(['./scss/**', './js/**'], ['styles:deploy', 'scripts:deploy'])
-})
+gulp.task('dev', (done) => series('clean', 'watch', done))
+gulp.task('default', (done) => series('clean', 'build', done))
