22b8d50b096e9cd087ec21ce114f0fe3ac804463
ah100101 <ah100101@gmail.com>
2017-10-30 18:49:38 -0500

    hooked up getter with tasks

--- src/js/components/List.vue
+++ src/js/components/List.vue
@@ -9,16 +9,16 @@
         taskform
         h3(v-html='title')
         section
-            div(v-for='task in tasks' 
+            div(v-for='task in allTasks' 
                 class = 'task')
                 button(v-on:click='completeTask(task)') Done
-                span(v-html='task')
+                span(v-html='task.text')
         br
         h3(v-html='completedTitle')
         section
             div(v-for='task in completedTasks' 
                 class = 'task')
-                span(v-html='task')
+                span(v-html='task.text')
 </template>
 
 <script>
@@ -27,7 +27,7 @@ import gql from 'graphql-tag'
 
 const TaskQuery = gql `
     query allTasks {
-      allPosts(orderBy: createdAt_DESC) {
+      allTasks(orderBy: text_DESC) {
         id
         text
         done
@@ -44,30 +44,30 @@ export default {
     }
   },
   apollo: {
-  //   allTasks: {
-  //     query: TaskQuery,
-  //     loadingKey: 'loading',
-  //   },
+    allTasks: {
+      query: TaskQuery,
+      loadingKey: 'loading',
+    },
   },
   components: {
     taskform: TaskForm
   },
   methods: {
-    completeTask: function(name) {
-      let tasksTodo = this.tasks.filter((task) => { return task !== name })
+    completeTask: function(completedTask) {
+      let tasksTodo = this.tasks.filter((task) => { return task.id !== completedTask.id })
       let tasksCompleted = this.completedTasks
-      tasksCompleted.push(name)
+      tasksCompleted.push(completedTask)
       this.$store.dispatch('setTasks', tasksTodo)
       this.$store.dispatch('setTasksCompleted', tasksCompleted)
     }
   },
   computed: {
-      tasks: function() {
-          return this.$store.getters.tasks.items
-      },
-      completedTasks: function() {
-          return this.$store.getters.tasks.completed
-      }
+    tasks: function() {
+        return this.$store.getters.tasks.items
+    },
+    completedTasks: function() {
+        return this.$store.getters.tasks.completed
+    }
   }
 }
 </script>
