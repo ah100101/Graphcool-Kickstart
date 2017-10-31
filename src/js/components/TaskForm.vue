c772c74f61b569b15a36fabe2a2b9b7c4974d647
ah100101 <ah100101@gmail.com>
2017-10-30 19:41:29 -0500

    hooked up mutation

--- src/js/components/TaskForm.vue
+++ src/js/components/TaskForm.vue
@@ -10,6 +10,18 @@
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
+const createTask = gql `
+    mutation createTask($text: String!, $done: Boolean!) {
+      createTask(text: $text, done: $done) {
+        id
+        text
+        done
+      }
+    }
+  `
+
 export default {
   data () {
     return {
@@ -18,10 +30,34 @@ export default {
     }
   },
   methods: {
-    addTask: function() {
+    addTask_old: function() {
       if(this.taskTitle !== '') {
         this.$store.dispatch('addTask', this.taskTitle)
         this.taskTitle = ''
+      }
+    },
+    addTask: function() {
+      if(this.taskTitle !== '') {
+
+        const text = this.taskTitle
+        const done = false
+
+        this.$apollo.mutate({
+          mutation: createTask,
+          variables: {
+            text,
+            done
+          }
+        })
+        .then(({data}) => {
+          //console.log(data.createTask)
+          // tasks currently not bound so this isn't going to work
+          this.$store.dispatch('addTask', data.createTask)
+          this.taskTitle = ''
+        })
+
+
+        // this.$store.dispatch('addTask', this.taskTitle)
       }
     }
   },
